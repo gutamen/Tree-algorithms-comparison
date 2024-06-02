4f4d785efa28155940a42c34d3c08ace1d29f6ca
Gustavo A Martini <gutamen@live.com>
2024-06-02 11:15:04 -0300

    separete class from excution

--- arvoreBinariaDeBusca.cpp
+++ arvoreBinariaDeBusca.cpp
@@ -2,66 +2,7 @@
 #include <fstream>
 #include <chrono>
 #include "BST.hpp"
-
-/*struct Node {
-    int key;
-    Node* left;
-    Node* right;
-    Node(int k) : key(k), left(nullptr), right(nullptr) {}
-};
-
-class BST {
-public:
-    BST() : root(nullptr), insertComparisons(0), searchComparisons(0) {}
-    void insert(int key);
-    bool search(int key);
-    int getInsertComparisons() const { return insertComparisons; }
-    int getSearchComparisons() const { return searchComparisons; }
-
-private:
-    Node* root;
-    int insertComparisons;
-    int searchComparisons;
-    Node* insertRec(Node* node, int key);
-    bool searchRec(Node* node, int key);
-};
-
-void BST::insert(int key) {
-    root = insertRec(root, key);
-}
-
-Node* BST::insertRec(Node* node, int key) {
-    if (node == nullptr) {
-        return new Node(key);
-    }
-    insertComparisons++;
-    if (key < node->key) {
-        node->left = insertRec(node->left, key);
-    } else {
-        node->right = insertRec(node->right, key);
-    }
-    return node;
-}
-
-bool BST::search(int key) {
-    searchComparisons = 0; // reset counter for each search
-    return searchRec(root, key);
-}
-
-bool BST::searchRec(Node* node, int key) {
-    if (node == nullptr) {
-        return false;
-    }
-    searchComparisons++;
-    if (key == node->key) {
-        return true;
-    } else if (key < node->key) {
-        return searchRec(node->left, key);
-    } else {
-        return searchRec(node->right, key);
-    }
-}
-*/
+#include "AVLTree.hpp"
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
